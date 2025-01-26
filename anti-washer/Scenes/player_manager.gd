5c79100cae0e8c4fc48f240163e4e44445b44b72
Eevaio <combinationeva@gmail.com>
2025-01-26 14:07:37 +0800

    ammo manager

--- anti-washer/Scenes/player_manager.gd
+++ anti-washer/Scenes/player_manager.gd
@@ -5,15 +5,31 @@ const RAY_LENGTH = 100
 @onready var sponge = preload("res://Scenes/sponge.tscn")
 @onready var playerHands = $Hand
 @onready var sponge_mesh = $Hand/SpongeMesh
+@onready var animHandler = $AnimationPlayer
+
+@export var can_throw = true
+
 var lastHit
 var tween : Tween
-var state: STATES = STATES.Idle
+@export var state: STATES = STATES.Idle
 enum STATES {Idle, Aiming}
 var raise_time = 0.3
 var lower_time = 0.3
 var topHPos = Vector3(-0.8, -0.6, 0)
 var botHPos = Vector3(-0.8, -1.2, 0)
+var cooldownTimer = 0.0
+const COOLDOWN = 0.4
 signal thrown_sponge
+
+func _ready() -> void:
+	cooldownTimer = COOLDOWN
+
+func _process(delta: float) -> void:
+	if !can_throw:
+		if cooldownTimer < COOLDOWN:
+			cooldownTimer += delta
+		elif !animHandler.is_playing():
+			animHandler.play("Reload")
 
 func _physics_process(delta: float) -> void:
 	var hit = raycast()
@@ -24,9 +40,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		hitMarker.visible = false
 
-var can_throw = true
-
-func throw_sponge(start, end):
+func throw_sponge():
 	var new = sponge.instantiate(PackedScene.GEN_EDIT_STATE_DISABLED)
 	new.add_to_group("Sponge")
 	new.position = global_position
@@ -34,36 +48,20 @@ func throw_sponge(start, end):
 	new.rotation = sponge_mesh.global_rotation
 	get_tree().get_root().add_child(new)
 	new.start = sponge_mesh.global_position
-	new.end = end
+	new.end = lastHit.position
+	can_throw = false
+	cooldownTimer = 0.0
 	emit_signal("thrown_sponge")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("throw"):
 		if !can_throw || state == STATES.Idle: return
-		throw_sponge(get_viewport().get_mouse_position(), lastHit.position)
-	elif event.is_action("aimhands"):
+		animHandler.play("Fire")
+	elif event.is_action("aimhands") && !animHandler.is_playing():
 		if event.is_action_pressed("aimhands"):
-			animate_hands(true)
+			animHandler.play("Ready")
 		elif event.is_action_released("aimhands"):
-			animate_hands(false)
-
-func animate_hands(raising):
-	if playerHands == null:
-		printerr("playerHands not found... skipping animatin")
-	if tween: tween.kill()
-	tween = get_tree().create_tween()
-	var npos = topHPos
-	var nt = raise_time
-	var ns = STATES.Aiming
-	if !raising:
-		npos = botHPos
-		nt = lower_time
-		ns = STATES.Idle
-	tween.tween_property(sponge_mesh, "visible", raising, 0.0)
-	tween.tween_property(playerHands, "position", npos, nt)
-	tween.tween_property(self, "state", ns, 0.0)
-	
-	tween.tween_callback(tween.kill)
+			animHandler.play_backwards("Ready")
 
 func raycast():
 	var space_state = get_world_3d().direct_space_state
